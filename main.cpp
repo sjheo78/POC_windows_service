16c61e56b410352872bee654f1c70c719697760c
sjheo78 <sjheo@78researchlab.com>
2023-06-21 13:35:23 +0900

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void install_service() {
+  return;
+}
+
+void uninstall_service() {
+  return;
+}
+
+void run_service() {
+  return;
+}
+
+void stop_service() {
+  return;
+}
+
+int main() {
+  install_service();
+  uninstall_service();
+  run_service();
+  stop_service();
+
+  return 0;
+}
